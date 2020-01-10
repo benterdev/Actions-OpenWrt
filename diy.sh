90b4fc25d5fc664ab0747a5dc834dd82c699d6be
benterdev <benterdev@outlook.com>
2020-01-11 01:48:39 +0800

    No Boot

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # M Def Passwd
 sed -i 's/root::0:0:99999:7:::/root:x7UukMvSvllDY:18176:0:99999:7:::/g' package/base-files/files/etc/shadow
+
+##Please Boot TT
