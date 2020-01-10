7265c8dfcfaf892a18590ce755099ed17ef62899
benterdev <benterdev@outlook.com>
2020-01-11 01:46:45 +0800

    a first

--- diy.sh
+++ diy.sh
@@ -6,4 +6,6 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# M Def Passwd
+sed -i 's/root::0:0:99999:7:::/root:x7UukMvSvllDY:18176:0:99999:7:::/g' package/base-files/files/etc/shadow
