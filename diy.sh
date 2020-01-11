45b4f252106982f78b49d3dfae36a571da4acb78
benterdev <benterdev@outlook.com>
2020-01-11 12:05:30 +0800

    add lang

--- diy.sh
+++ diy.sh
@@ -7,7 +7,5 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-# M Def Passwd
-sed -i 's/root::0:0:99999:7:::/root:x7UukMvSvllDY:18176:0:99999:7:::/g' package/base-files/files/etc/shadow
 
 ##Please Boot TT
